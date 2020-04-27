42206bf630cfb81ee0945480c1e3768bd5ce2c6b
samson92-w <54155852+samson92-w@users.noreply.github.com>
2020-04-27 18:57:47 -0400

    jira-c110 adding another sysout line

--- com.lerninggit/src/test/java/com/lerninggit/HellowWorld.java
+++ com.lerninggit/src/test/java/com/lerninggit/HellowWorld.java
@@ -1,9 +1,11 @@
-package com.lerninggit;
+ package com.lerninggit;
 
 public class HellowWorld {
 
 	public static void main(String[] args) {
+		
 		System.out.println("hellow git hub");
+		System.out.println("hellow after commited");
 
 	}
 
